0121d1d3d4e4626062caedc481adb8df089d4d36
seed-fe <zzlfullstack@gmail.com>
2017-02-26 20:04:56 +0800

    用字面量语法重写了原型对象

--- src/js/backtop.js
+++ src/js/backtop.js
@@ -8,6 +8,7 @@ define(['jquery', 'scrollto'], function($, scrollto) {
             speed: this.opts.speed
         })
         if (this.opts.mode == 'move') {
+            // jquery的proxy方法相当于原生的bind方法，改变函数的执行环境，这里this原本是指向BackTop的实例，调用proxy方法后指向this.$el
             this.$el.on('click', $.proxy(this._move, this));
         } else {
             this.$el.on('click', $.proxy(this._go, this));
@@ -19,7 +20,7 @@ define(['jquery', 'scrollto'], function($, scrollto) {
         pos: $(window).height(),
         speed: 800
     }
-    BackTop.prototype._move = function() {
+    /*BackTop.prototype._move = function() {
         this.scroll.move();
     }
     BackTop.prototype._go = function() {
@@ -32,7 +33,26 @@ define(['jquery', 'scrollto'], function($, scrollto) {
         } else {
             $el.fadeOut();
         }
+    }*/
+    /*用字面量语法重写原型对象*/
+    BackTop.prototype = {
+        constructor: BackTop,
+        _move: function() {
+            this.scroll.move();        
+        },
+        _go: function() {
+            this.scroll.go();       
+        },
+        _checkPosition: function() {
+            var $el = this.$el;
+            if ($(window).scrollTop() > this.opts.pos) {
+                $el.fadeIn().css('display', 'block');
+            } else {
+                $el.fadeOut();
+            }
+        }
     }
+    // 将一个对象添加到jquery的原型上从而提供新的jquery实例方法，这里就相当于给jquery实例对象提供了一个backtop方法，这也是实现jquery插件的常用方法
     $.fn.extend({
         backtop: function(opts) {
             // return 是为了实现连缀
