f2ab6d121ce98de013bb9567b8736774bc319e93
Bill Zhang <zzlfullstack@gmail.com>
2017-05-10 14:18:32 +0800

    isShow变量也可以定义在backtop模块函数里
    * 添加了一些注释
    * 删除了console语句
    * 在backtop模块的函数里定义了isShow变量

--- src/script/backtop.js
+++ src/script/backtop.js
@@ -1,11 +1,12 @@
 define(['jquery'], function($) {
     // 不用scrollto模块了，直接在move和go方法中操作html和body的scrollTop
     var scrollObject = $('html, body');
+    var isShow = false;
     function BackTop (el, opts) {
         // body...
         this.opts = $.extend({}, BackTop.DEFAULTS, opts);
         this.$el = $(el);
-        this.isShow = false;
+        // this.isShow = false; // 表示按钮是否显示的状态变量，默认是不显示，所以初始为false
         if (this.opts.mode == 'move') {
             // jquery的proxy方法相当于原生的bind方法，改变函数的执行环境，这里this原本是指向BackTop的实例(因为是调用BackTop的原型方法)，调用proxy方法后指向this.$el
             this.$el.on('click', $.proxy(this._move, this));
@@ -13,7 +14,6 @@ define(['jquery'], function($) {
             this.$el.on('click', $.proxy(this._go, this));
         }
         $(window).on('scroll', $.proxy(this._checkPosition, this));
-        // this._checkPositionOnload();
         this._checkPosition();
     }
     BackTop.DEFAULTS = {
@@ -25,33 +25,14 @@ define(['jquery'], function($) {
         pos: $(window).height(),
         speed: 800
     }
-    /*BackTop.prototype._move = function() {
-        this.scroll.move();
-    }
-    BackTop.prototype._go = function() {
-        this.scroll.go();
-    }
-    BackTop.prototype._checkPosition = function() {
-        var $el = this.$el;
-        if ($(window).scrollTop() > this.opts.pos) {
-            $el.fadeIn().css('display', 'block');
-        } else {
-            $el.fadeOut();
-        }
-    }*/
     /*用字面量语法重写原型对象*/
     BackTop.prototype = {
         constructor: BackTop,
         _move: function() {
-            // console.log('move executed~')
             var opts = this.opts,
             dest = opts.dest;
-            // console.log(this)
-            // console.log(dest);
             if ($(window).scrollTop() != dest) {
                 if (!scrollObject.is(':animated')) {
-                    // console.log(1);
-                    // console.log(scrollObject);
                     scrollObject.animate({
                         scrollTop: dest
                     }, opts.speed);
@@ -65,29 +46,21 @@ define(['jquery'], function($) {
             }       
         },
         _checkPosition: function() {
-            // console.log('checkPosition executed~');
             var $el = this.$el;
             if ($(window).scrollTop() > this.opts.pos) {
-                if (!this.isShow) {
+                if (!isShow) {
                     $el.fadeIn().css('display', 'block');
-                    this.isShow = true;
+                    isShow = true;
                 }
                 
             } else {
-                if (this.isShow) {
+                if (isShow) {
                     $el.fadeOut();
-                    this.isShow = false;
+                    isShow = false;
                 }
                 
             }
         }
-        /*,
-        _checkPositionOnload: function () {
-            var $el = this.$el;
-            if ($(window).scrollTop() > this.opts.pos) {
-                $el.css('display', 'block');
-            }
-        }*/
     }
     // 将一个对象添加到jquery的原型上从而提供新的jquery实例方法，这里就相当于给jquery实例对象提供了一个backtop方法，这也是实现jquery插件的常用方法，对象里定义的方法名就是插件方法名
     $.fn.extend({
